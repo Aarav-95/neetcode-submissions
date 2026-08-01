a45749ace8b071157a0fe8346c9fb6ae2b46cbba
Aarav Patel <128869545+Aarav-95@users.noreply.github.com>
2026-08-01 16:19:30 -0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        curr = []
+        n = len(s)
+        def back(idx):
+            if idx == n:
+                res.append(curr.copy())
+            for i in range(idx, n):
+                t = s[idx:i+1]
+                print(curr)
+                if t == t[::-1]:
+                    curr.append(t)
+                    back(i+1)
+                    curr.pop()
+        
+        back(0)
+        return res
